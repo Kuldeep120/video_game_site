68634dc2a37d99e2a856be809ac2277412daf590
Kuldeep Kumar <kuldeepkhatri03@gmail.com>
2025-05-20 23:57:15 +0500

    Used react icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,29 @@
 import { useState } from "react";
 import Alert from "./components/Alert";
-import "./App.css"
+import "./App.css";
 // import Button from "./components/Button";
 import ListGroup from "./components/ListGroup";
+import { BsFillCalendarHeartFill } from "react-icons/bs";
 
 function App() {
-  const items = ["Karachi","Lahore", "Quetta","Peshawar","Islamabad"]
-  const [alertVisible, setAlertVisible] = useState(false)
+  const items = ["Karachi", "Lahore", "Quetta", "Peshawar", "Islamabad"];
+  const [alertVisible, setAlertVisible] = useState(false);
   // const handleOnClick = ()=> console.log("buttons clicked")
-  const handleSelectItem = (item: string) => {console.log(item)}
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
+  return ( <div><BsFillCalendarHeartFill color="red" /></div>);
   return (
     <div>
-      {alertVisible && <Alert onClose={()=>setAlertVisible(false)}>Hidder Alert</Alert>}
+      {alertVisible && (
+        <Alert onClose={() => setAlertVisible(false)}>Hidder Alert</Alert>
+      )}
       {/* <Button onClick={()=>setAlertVisible(true)}>MyButton</Button> */}
-      <ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem}></ListGroup>
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={handleSelectItem}
+      ></ListGroup>
     </div>
   );
 }
