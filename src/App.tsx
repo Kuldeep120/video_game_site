d3e342e8157387bc8732b1e1c4601fecad44b16d
Kuldeep Kumar <kuldeepkhatri03@gmail.com>
2025-05-22 01:12:27 +0500

    state updating the subitem in object state

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,26 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [pizza, setPizza]= useState({
-    name: "Spicy Papperoni",
-    toppings: ["Mushroom", "Olives"]
-  })
-  console.log(pizza)
+  const [cart, setCart] = useState({
+    discount: 0.1,
+    items: [
+      {
+        id: 1,
+        product: "Product 1",
+        qty: 1,
+      },
+      {
+        id: 2,
+        product: "Product 2",
+        qty: 1,
+      },
+    ],
+  });
+  console.log(cart);
   const handleClick = () => {
-    const newPizza = { ...pizza, toppings: [...pizza.toppings, "Tommatoes","Sweet Corn"]}
-    setPizza(newPizza)
-  }
+    const newCart = {...cart,items: cart.items.map((item)=> item.id===1?{...item, qty:2}:item)};
+    setCart(newCart);
+  };
   return (
     <div>
       <button onClick={handleClick}>Click</button>
