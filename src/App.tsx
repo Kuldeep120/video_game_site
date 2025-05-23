fcc8c51d981e051800ad47f934ace43521a5fbce
kuldeep_kumar75 <kuldeep.kumar@campana.com>
2025-05-23 12:02:12 +0500

    added state sharing between component

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,22 @@
 import { useState } from "react";
 import "./App.css";
 import { produce } from "immer";
+import Cart from "./components/Cart";
+import NavBar from "./components/NavBar";
 
 function App() {
-  const [cart, setCart] = useState({
-    discount: 0.1,
-    items: [
-      {
-        id: 1,
-        product: "Product 1",
-        qty: 1,
-      },
-      {
-        id: 2,
-        product: "Product 2",
-        qty: 1,
-      },
-    ],
-  });
-  console.log(cart);
-  const handleClick = () => {
-    const newCart = produce((draft) => {
-      const item1 = draft.items.find((item) => item.id === 1);
-      item1.qty++;
-    });
-    setCart(newCart);
-  };
+  const [cartItems, setcartItems] = useState(["Product 1", "Product 2"]);
+  const onHandleClick = ()=>{
+    const newCart = [...cartItems, "Product "+(cartItems.length+1)  ]
+    setcartItems(newCart)
+  } 
+  const handleClear = () => {
+    setcartItems([])
+  }
   return (
     <div>
-      <button onClick={handleClick}>Click</button>
+      <NavBar cartItemsCount={cartItems.length }/>
+      <Cart cartItems={cartItems} addItemtoCart={onHandleClick} onClear={handleClear} />
     </div>
   );
 }
